dd5ae19d144bff0707452f642e087e1e4ba78311
herdeybayor <shereefdeen4real@gmail.com>
2023-11-06 23:28:42 +0100

    chore: seed random number

--- menu.cpp
+++ menu.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 void print_array(std::vector<std::string>);
+void play_game();
 
 int main()
 {
+	srand(time(NULL));
 	int choice;
 	std::vector<std::string> menu = {"0. Quit", "1. Play Game", "2. Load Game", "3. Play Multiplayer", "4. Options", "5. Cheat"};
 
@@ -21,7 +25,7 @@ int main()
 			std::cout << "Quitting game..." << std::endl;
 			break;
 		case 1:
-			std::cout << "Starting game..." << std::endl;
+			play_game();
 			break;
 		case 2:
 			std::cout << "Loading game..." << std::endl;
@@ -50,4 +54,43 @@ void print_array(std::vector<std::string> array)
 	{
 		std::cout << array[i] << std::endl;
 	}
+}
+
+void play_game()
+{
+	int random = rand() % 251;
+	std::vector<int> guesses;
+
+	// std::cout << random << std::endl;
+
+	while (true)
+	{
+		int guess;
+		std::cout << "Guess a number: ";
+		std::cin >> guess;
+
+		guesses.push_back(guess);
+
+		if (guess == random)
+		{
+			std::cout << "You win!" << std::endl;
+			break;
+		}
+		else if (guess < random)
+		{
+			std::cout << "Too low" << std::endl;
+		}
+		else
+		{
+			std::cout << "Too high" << std::endl;
+		}
+	}
+
+	std::cout << "You guessed: " << guesses.size() << " times" << std::endl;
+
+	for (int i = 0; i < guesses.size(); i++)
+	{
+		std::cout << guesses[i] << "\t";
+	}
+	std::cout << std::endl;
 }
